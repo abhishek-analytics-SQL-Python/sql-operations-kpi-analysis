8a6b1f413c7604e049477dec5092efdb9fe5363e
abhishek-analytics-SQL-Python <abhishekdutta337@gmail.com>
2025-12-28 22:01:23 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO agents VALUES
+(1, 'Ravi', 'Mumbai'),
+(2, 'Ankit', 'Delhi'),
+(3, 'Suresh', 'Bangalore');
+
+INSERT INTO orders VALUES
+(101, '2024-01-01', '2024-01-03', 1, 'Delivered', 'Mumbai'),
+(102, '2024-01-05', '2024-01-10', 1, 'Delayed', 'Mumbai'),
+(103, '2024-01-07', '2024-01-08', 2, 'Delivered', 'Delhi'),
+(104, '2024-02-01', '2024-02-05', 3, 'Delayed', 'Bangalore'),
+(105, '2024-02-10', '2024-02-11', 2, 'Delivered', 'Delhi')
